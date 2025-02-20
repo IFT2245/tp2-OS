953882c5b43fe90bb5800aa4c91f162da2a670b0
spokboud <sam.blouin@umontreal.ca>
2025-02-20 22:11:56 +0000

    git ignore set

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/spokboud/Course-TP2-Solutions
-# Build directory: /home/spokboud/Course-TP2-Solutions/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/spokboud/Course-TP2-Solutions/src
-# Build directory: /home/spokboud/Course-TP2-Solutions/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
